0f0cdc2a9f5c952d87334526f13a8d4da5f11249
Raciel Garcia <rgarcia.rg70@gmail.com>
2021-02-06 16:00:55 -0500

    Added User repo interface

--- src/main/java/com/raciel/repositories/UserRepository.java
+++ src/main/java/com/raciel/repositories/UserRepository.java
@@ -2,8 +2,7 @@ package com.raciel.repositories;
 
 import com.raciel.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-/*@Repository
-public class UserRepository extends JpaRepository<UserEntity, Long> {
-}*/
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    
+}
