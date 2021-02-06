7c80b1274101fb7eab3f12ab81494963fcb231a2
Raciel Garcia <rgarcia.rg70@gmail.com>
2021-02-05 20:17:36 -0500

    added jpa anf h2 database

---
+++ src/main/java/com/raciel/entities/UserEntity.java
@@ -0,0 +1,7 @@
+package com.raciel.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UserEntity {
+}
---
+++ src/main/java/com/raciel/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.raciel.repositories;
+
+import com.raciel.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*@Repository
+public class UserRepository extends JpaRepository<UserEntity, Long> {
+}*/
